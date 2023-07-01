17d54a3008fea8f3f127173ecee3fd6571ace7bc
David Portilla <dportillalara@gmail.com>
2023-06-30 21:06:50 -0400

    added screens and export b

---
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ApptutorialScreen extends StatelessWidget {
+  static const String name = 'apptutorial_screen';
+  const ApptutorialScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class UiControlScreen extends StatelessWidget {
+  static const String name = 'ui_control_screen';
+  const UiControlScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
